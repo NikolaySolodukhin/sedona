0d1649c7616b1c2bb0d2e9c5c03a57a715699800
Nikolay Solodukhin <Nikolay.Solodukhin@gmail.com>
2017-07-11 00:03:46 +0300

    Настроил сборку. Исправил ошибки БЭМа. Удалил иконочный шрифт

--- js/script.js
+++ js/script.js
@@ -1,27 +1,35 @@
-var buttonChoose = document.querySelector('.promo__search');
-var form = document.querySelector('.search-hotels');
-if (document.getElementById('search')){
-form.classList.add('search-hotels__hidden');
-};
-buttonChoose.onclick = function() {
-  if (form.classList.contains('search-hotels__hidden')) {
-    form.classList.remove('search-hotels__hidden');
-  } else {
+'use strict';
+
+window.utils = (function() {
+  var buttonChoose = document.querySelector('.promo__search');
+  var form = document.querySelector('.search-hotels');
+  if (document.getElementById('search')) {
     form.classList.add('search-hotels__hidden');
   }
-};
-ymaps.ready(function () {
-  var myMap = new ymaps.Map('map-canvas', {
-      center: [34.8697395, -111.7609896],
-      zoom: 12
-    }, {
-      searchControlProvider: 'yandex#search'
-    }),
-    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-      balloonContent: 'Sedona'
-    }, {
-      iconLayout: 'default#image',
-    });
 
-  myMap.geoObjects.add(myPlacemark);
-});
+  buttonChoose.onclick = function() {
+    if (form.classList.contains('search-hotels__hidden')) {
+      form.classList.remove('search-hotels__hidden');
+    } else {
+      form.classList.add('search-hotels__hidden');
+    }
+  };
+
+  ymaps.ready(function() {
+    var myMap = new ymaps.Map('map-canvas', {
+        center: [34.8697395, -111.7609896],
+        zoom: 12
+      }, {
+        searchControlProvider: 'yandex#search'
+      }),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+        balloonContent: 'Sedona'
+      }, {
+        iconLayout: 'default#image',
+      });
+
+    myMap.geoObjects.add(myPlacemark);
+  });
+
+})();
